aeb28485f3bebc00404716e1e3bc48e3d3d38bb0
MYSHOGUN <www.789946@hotmail.com>
2025-04-25 22:20:51 +0700

    sidebar 3rd

--- server.js
+++ server.js
@@ -13,42 +13,50 @@ app.set("views", path.join(__dirname, "app1", "public"));
 app.use(express.static(path.join(__dirname, "app1", "src")));
 
 // ฟังก์ชันช่วย render พร้อม layout
-function renderWithLayout(res, view, data = {}) {
-  ejs.renderFile(path.join(__dirname, "app1", "public", `${view}.ejs`), data, (err, str) => {
+function renderWithLayout(res, view, data = {}, reqPath = "") {
+  ejs.renderFile(path.join(__dirname, "app1", "public", `${view}.ejs`), { ...data, currentPath: reqPath }, (err, str) => {
     if (err) {
       res.status(500).send(err.message);
     } else if(view === 'login'){
       return res.render(view, data);
     }else {
-      res.render("layout", { ...data, body: str });
+      res.render("layout", { ...data, body: str, currentPath: reqPath });
     }
   });
 }
 
 // เส้นทางหน้าหลัก
 app.get("/", (req, res) => {
-  renderWithLayout(res, "index", { title: "KMUTNB Project - Main" });
+  renderWithLayout(res, "index", { title: "KMUTNB Project - Main" }, req.path);
 });
 
 app.get("/upload", (req, res) => {
-  renderWithLayout(res, "upload", { title: "KMUTNB Project - Upload" });
+  renderWithLayout(res, "upload", { title: "KMUTNB Project - Upload" }, req.path);
 });
 
 app.get("/status", (req, res) => {
-  renderWithLayout(res, "status", { title: "KMUTNB Project - Status" });
+  renderWithLayout(res, "status", { title: "KMUTNB Project - Status" }, req.path);
 });
 
 app.get("/login", (req, res) => {
-  renderWithLayout(res, "login", { title: "KMUTNB Project - Login" });
+  renderWithLayout(res, "login", { title: "KMUTNB Project - Login" }, req.path);
 });
 
 app.get("/flowchart", (req, res) => {
-  renderWithLayout(res, "flowchart", { title: "KMUTNB Project - Flowchart" });
+  renderWithLayout(res, "flowchart", { title: "KMUTNB Project - Flowchart" }, req.path);
+});
+
+app.get("/file", (req, res) => {
+  renderWithLayout(res, "file", { title: "KMUTNB Project - File" }, req.path);
+});
+
+app.get("/chat", (req, res) => {
+  renderWithLayout(res, "chat", { title: "KMUTNB Project - Chat" }, req.path);
 });
 
 // เส้นทางหน้าโปรไฟล์
 app.get("/profile", (req, res) => {
-  renderWithLayout(res, "profile", { title: "Profile" });
+  renderWithLayout(res, "profile", { title: "Profile" }, req.path);
 });
 
 // API
