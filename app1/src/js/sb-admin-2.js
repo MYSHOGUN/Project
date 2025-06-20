636ff7336175b1f47b8cdafe9a43a968ca3e332c
MYSHOGUN <www.789946@hotmail.com>
2025-06-20 21:53:45 +0700

    flowchart

--- app1/src/js/sb-admin-2.js
+++ app1/src/js/sb-admin-2.js
@@ -12,10 +12,16 @@
 
   // Close any open menu accordions when window is resized below 768px
   $(window).resize(function() {
+    if (($(window).width() >= 768) && $(".sidebar").hasClass("toggled")){
+      $("body").removeClass("sidebar-toggled");
+      $(".sidebar").removeClass("toggled");
+      $('.sidebar .collapse').collapse('hide');
+    };
+
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
     };
-    
+
     // Toggle the side navigation when window is resized below 480px
     if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
       $("body").addClass("sidebar-toggled");
